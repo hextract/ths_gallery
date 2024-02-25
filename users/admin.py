95953cdbbd79c767d06384b2f86fd168fe4b0489
h4x4d <sidenkoleg@gmail.com>
2024-02-25 11:06:18 +0300

    v1.3.1 admin changes

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from users.models import Account
 
 
-admin.site.register(Account)
+@admin.register(Account)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'is_superuser', 'is_staff', 'can_vote')

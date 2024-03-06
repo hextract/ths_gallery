9d2b80f6af3a4ab3ac9a52cce2a098e1c0ec8cbc
h4x4d <sidenkoleg@gmail.com>
2024-03-06 11:38:29 +0300

    v1.4.1 statistics

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,10 @@ from users.models import Account
 
 @admin.register(Account)
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'is_superuser', 'is_staff', 'can_vote')
+    list_display = ('__str__', 'is_superuser', 'is_staff', 'can_vote', 'votes', 'average')
+
+    def votes(self, obj):
+        return obj.votes.count()
+
+    def average(self, obj):
+        return sum(i.result for i in obj.votes.all()) // max(obj.votes.count(), 1)

2412676dd5e53af2e7315ec3776bd82b69b1d99e
h4x4d <sidenkoleg@gmail.com>
2024-07-27 12:58:35 +0300

    add cors

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
 
     "crispy_forms",
     "crispy_bootstrap5",
+
+    "corsheaders"
 ]
 
 AUTH_USER_MODEL = 'users.Account'
@@ -38,6 +40,7 @@ AUTH_USER_MODEL = 'users.Account'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
